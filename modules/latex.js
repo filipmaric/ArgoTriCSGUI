5901d08476d41b58f442128967084b7bf20bd0d7
Filip Marić <filip@matf.bg.ac.rs>
2022-05-10 22:17:21 +0200

    initial commit

--- modules/latex.js
+++
@@ -1,9 +0,0 @@
-function laTeX2HTML(str) {
-    return str ? str.replace(/([a-zA-Z_']+)_{(\w+)}/g, '$1<sub>$2</sub>') : "";
-}
-
-function removeLaTeX(txt) {
-    return txt.replace(/([\w']+)_{(\w+)}/, '$1$2');
-}
-
-export { laTeX2HTML, removeLaTeX };
